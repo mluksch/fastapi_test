7eae796bc40799595638b10e1cf23262a5b1c5e5
mluk <mikauschkal@gmail.com>
2022-02-13 22:40:28 +0100

    wip

--- main.py
+++ main.py
@@ -40,18 +40,24 @@ def index():
 
 
 # Catch GET/POST-Parameters directly with arguments in request-handlers
-# Example: http://localhost:8000/items?filter=m&limit=2
-# Returns: ["Jeremy","Max"]
+# Example: http://localhost:8000/persons?filter=j&limit=2&orderby=age
+# Returns: [{"name":"Judy","age":10},{"name":"Jeremy","age":20}]
 @app.get("/persons")
-def items(filter: str = "", limit: int = 10):
+def items(filter: str = "", limit: int = 10, orderby: str = "name"):
     # explicitly encode objects to json-response with FastAPI:
-    filtered = [p for p in persons if filter in p.get("name").lower()][0:limit]
+    # builtin-function "sorted" returns new list
+    filtered = sorted([p for p in persons
+                       if filter in p.get("name").lower()][0:limit],
+                      key=lambda p: p[orderby])
     return encoders.jsonable_encoder(filtered)
 
 
 # Catch Path-Parameters with "/../{key}" pattern in the path
-@app.get("/person/{name}")
+# Example: http://localhost:8000/persons/jack
+# Returns: {"name":"Jack","age":80}
+@app.get("/persons/{name}")
 def add_item(name: str):
-    # get first element by predicate in list using a generator:
+    # use a generator in order to get the first element
+    # matching a predicate in a list:
     generator = (p for p in persons if p.get("name").lower() == name)
     return next(generator, None)
